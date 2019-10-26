1f49a3355685ffe59ab5c66ff94d880d47577974
Nitesh Kumar <nitesh.kumar@tamu.edu>
2019-10-26 14:11:21 -0500

    If applied, this commit creates main matching function (match.cov) skeleton.

---
+++ R/matching.R
@@ -0,0 +1,11 @@
+######### function to match data set##################
+# match.cov : main matching function given data sets
+# ref : data set considered as a reference
+# obj : data set whose each observation searches for a matching observation in ref
+# thres : threshold against which similarity is obtained between ref and obj data sets
+# circ_pos : position of circular variables (such as wind direction etc), if supplied by user
+match.cov = function(ref, obj, thres, circ_pos = NULL){
+
+}
+
+
