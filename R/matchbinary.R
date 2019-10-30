db398c601cdf73aeef7cce0d1a7d3e7aac7d32a5
Nitesh Kumar <nitesh.kumar@tamu.edu>
2019-10-29 23:52:59 -0500

    If applied, this commit inserts covmatch.binary function and roxygen skeleton.

---
+++ R/matchbinary.R
@@ -0,0 +1,15 @@
+#' Title
+#'
+#' @param dname
+#' @param cov
+#' @param weight
+#' @param cov_circ
+#'
+#' @return
+#' @export
+#'
+#' @examples
+covmatch.binary = function(dname, cov, weight = 0.2, cov_circ = NULL ){
+
+
+}
