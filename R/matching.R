4b62dc848848c0e6fef4a2a759cbdb3b4a13c263
Nitesh Kumar <nitesh.kumar@tamu.edu>
2019-11-19 07:53:55 -0600

    If applied, this commit replaces R matching code with C++ matching function.

--- R/matching.R
+++ R/matching.R
@@ -4,82 +4,13 @@
 # obj : data set whose each observation searches for a matching observation in ref
 # thres : threshold against which similarity is obtained between ref and obj data sets
 # circ_pos : position of circular variables (such as wind direction etc), if supplied by user
-match.cov = function(ref, obj, thres, circ_pos = NULL){
+match.cov = function(ref, obj, thres, circ_pos = 0, flag = 0){
 
   # Ensuring that the data sets are converted to matrix
   ref = as.matrix(ref)
   obj = as.matrix(obj)
 
-  # vector to store matched index of observation in obj
-  match = rep(NA, nrow(ref))
-
-  # vector to store index of obj set
-  index = c(1:nrow(obj))
-
-  # Loop to iterate over each ref observation
-  for(i in 1:nrow(ref)){
-
-       # Calculating ratio between obj set and each ref observation
-       ref.i = as.numeric(ref[i, ])
-       score = sweep(abs(sweep(obj, 2, ref.i, FUN = '-')), 2, ref.i, FUN = '/')
-
-       # Adjusting the score of circular
-       if(length(circ_pos > 0)){
-
-         # Manipulating the circular variable difference to fit in the range 0 to 180
-         id.dcor = which(abs(obj[, circ_pos, drop = F] - ref.i[circ_pos]) > 180)
-         score[id.dcor, circ_pos] = (360 - abs(obj[id.dcor, circ_pos, drop = F] - ref.i[circ_pos])) / ref.i[circ_pos]
-
-       }
-
-       # Checking which observation in test set matches reference set
-       id = which(rowSums(sweep(score, 2, thres, FUN = '<')) == ncol(ref))
-
-       # Checking for multiple matches
-       if(length(id) > 0){
-
-         # Adjusted score calculated when multiple matches
-         scoreadj = sweep(score, 2, thres, FUN = '/')
-
-         # Calculating max adjusted score for matched index
-         maxscore = rowMaxs(scoreadj[id, , drop = F])
-
-         # Filterig index with min score from maxscore
-         id = id[which.min(maxscore)]
-         colNo = 1
-
-         if(ncol(ref) == 1){
-           while(length(id) > 1){
-
-             # Filtering index with min score if still multiple matches exists
-             score.id = t(apply(scoreadj[id, , drop = F], 1, function(x) sort(x, decreasing = T)))
-             id = which(score.id[ , colNo, drop = F] == min(score.id[ , colNo, drop = F]))
-
-           }
-
-         }else{
-
-           while(length(id)>1){
-
-             # Filtering index with min score if still multiple matches exists
-             colNo = colNo+1
-             score.id = t(apply(scoreadj[id, , drop = F], 1, function(x) sort(x, decreasing = T)))
-             id = which(score.id[, colNo, drop=F] == min(score.id[, colNo, drop = F]))
-
-            }
-         }
-
-       }
-
-       # Storing matched index of obj set
-       match[i] = index[id]
-
-       # Removing matched index observation from obj set
-       obj = obj[-id, , drop=F]
-
-       # Removing matched index of obj set
-       index = index[-id]
-  }
+  match = matchcov(ref = ref , obj = obj, thres = thres, circ_pos = circ_pos, flag = flag)
 
   # Returns matched index
   return(match)
