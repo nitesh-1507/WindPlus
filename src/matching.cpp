97a3b8ac11975c50f8b64554ad571fb7fe7033d0
Nitesh Kumar <nitesh.kumar@tamu.edu>
2019-11-18 22:37:07 -0600

    If applied, this commit creates a skeleton for C++.

---
+++ src/matching.cpp
@@ -0,0 +1,11 @@
+#include <RcppArmadillo.h>
+// [[Rcpp::depends(RcppArmadillo)]]
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+arma::vec matchcov(arma::mat& ref , arma::mat& obj, arma::rowvec& thres, arma::rowvec& circ_pos, int pos)
+{
+
+
+
+}
